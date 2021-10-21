d8d276ebe2ed4d7319a09975428c5a259f5f03fd
Sebastian <sebas5384@gmail.com>
2021-10-21 02:14:08 +0000

    first schema

---
+++ src/index.ts
@@ -0,0 +1,36 @@
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+async function main() {
+  // await prisma.user.create({
+  //   data: {
+  //     name: 'Alice',
+  //     email: 'alice@prisma.io',
+  //     posts: {
+  //       create: { title: 'Este es el titulo'}
+  //     },
+  //     profile: {
+  //       create: { bio: 'I like turtles'}
+  //     }
+  //   }
+  // })
+
+  // await prisma.user.deleteMany()
+
+  const allUsers = await prisma.user.findMany({
+    include: {
+      posts: true,
+      profile: true
+    }
+  })
+  console.log(JSON.stringify(allUsers))
+}
+
+main()
+  .catch((e) => {
+    throw e
+  })
+  .finally(async () => {
+    await prisma.$disconnect()
+  })
